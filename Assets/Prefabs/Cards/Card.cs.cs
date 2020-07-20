11d19be649a7bf3192f9baa8b33c7ed7d2b86a82
BrandedTales <lordbrand@gmail.com>
2020-07-20 00:03:20 -0500

    0. Initial setup.

---
+++ Assets/Prefabs/Cards/Card.cs.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BT.CrystalBright {
+    public class Card : MonoBehaviour
+    {
+        public enum CardType { Martial, Magic, Fairy }
+    }
+}
